fab4f9f6737d3dbe47128f535974a9b139b2b911
Alvaro Neira <alvaroneira98@gmail.com>
2025-07-01 11:39:43 -0500

    loopstudios landing page footer and fix creations

---
+++ loopstudios-landing-page/src/navbaroptions/navbaroptions.ts
@@ -0,0 +1,24 @@
+const navbarOptions = [
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Careers",
+    href: "/careers",
+  },
+  {
+    name: "Events",
+    href: "/events",
+  },
+  {
+    name: "Products",
+    href: "/products",
+  },
+  {
+    name: "Support",
+    href: "/support",
+  },
+];
+
+export default navbarOptions;
