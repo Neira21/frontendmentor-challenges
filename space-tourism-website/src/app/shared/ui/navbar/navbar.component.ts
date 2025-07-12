7b67949d685b00f3c5b95c7fd00b0d231fcb1242
Alvaro Neira <alvaroneira98@gmail.com>
2025-07-11 21:04:16 -0500

    space tourism, fix navbar, add home page

--- space-tourism-website/src/app/shared/ui/navbar/navbar.component.ts
+++ space-tourism-website/src/app/shared/ui/navbar/navbar.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  @Input() isOpen = false;
+
+  @Output() closeNavbar = new EventEmitter<void>(); // ✅ Emisor de evento
 
 }
